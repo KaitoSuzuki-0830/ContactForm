b1e03c5b9d0b43633277cec397d7be4807fedb51
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2019-10-04 11:52:57 +0530

    first commit

---
+++ includes/mail.php
@@ -0,0 +1,17 @@
+<?php
+return [
+
+  'driver' => 'smtp',
+  'smtp' => [
+    'host' => 'smtp.mailtrap.io',
+    'port' => 2525,
+    'from' => [
+            'address' =>'kaito.083097@gmail.com',
+            'name' => 'Kaito'
+          ],
+    'username' => 'f1b95aec6834fe',
+    'password' => '6b012857669885',
+    'timeout' => 5
+    ]
+  ];
+?>
